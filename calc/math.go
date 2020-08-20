2b8b03fded51a86e983cb1aec312afe1c4526d66
bogdan <bogdan@reynencourt.com>
2020-08-20 13:25:53 +0200

    the Add() function now accepts variadic arguments

--- calc/math.go
+++ calc/math.go
@@ -1,7 +1,14 @@
 package calc
 
 // math.go is located inside the calc/ package to provide utility functions, such as Add();
-func Add(i, j int) int {
-	return i + j
-}
+func Add(numbers ...int) int {
 
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+
+}
